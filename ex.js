539bd685d56d8a089e14ecfa003aa51fbe9ec9ea
James-Jamshid <jamshid.makh94@gmail.com>
2023-02-09 15:39:58 +0900

    while, function

---
+++ ex.js
@@ -0,0 +1,18 @@
+//karra jadvali masalasi
+
+// for (i = 2; i <= 9; i++) {
+//   console.log("2lik karra jadvali")
+//   for (b = 2; b <= 9; b++) {
+//     console.log(b)
+//   }
+//   console.log(`${i} x `)
+// }
+
+//n ta raqam ichidan prime(oziga va 1ga bolinuchi) sonlarni topish)
+//
+
+for (i = 0; i <= 10; i++) {
+  if (i % i == 0 && i % 2 !== 0) {
+    console.log(i)
+  }
+}
